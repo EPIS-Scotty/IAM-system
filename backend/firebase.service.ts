1024fc1077a83c467bb89aeaa8c5c95fb07b265c
Vorapon Kachapornkul <hakao97@gmail.com>
2024-10-16 18:14:21 +0700

    Connect first CRUD with firestore

--- backend/firebase.service.ts
+++ backend/firebase.service.ts
@@ -1,6 +1,8 @@
 import * as admin from 'firebase-admin';
 import { Injectable } from '@nestjs/common';
 
+var serviceAccount = require("epis-scotty-firebase-adminsdk-zs1ev-fc840252c3.json");
+
 @Injectable()
 export class FirebaseService {
   private firestore: admin.firestore.Firestore;
@@ -8,13 +10,14 @@ export class FirebaseService {
   constructor() {
     if (!admin.apps.length) {
       admin.initializeApp({
-        credential: admin.credential.applicationDefault(),
+        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+        databaseURL: 'https://459881385193.firebaseio.com', // Replace <your-project-id>
       });
     }
     this.firestore = admin.firestore();
   }
 
-  getFirestore() {
+  getFirestore(): admin.firestore.Firestore {
     return this.firestore;
   }
 }
